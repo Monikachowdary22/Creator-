c7fba5a6539072e69e68ce701e7a32a773c662db
Monikachowdary22 <monikachowdary2203@gmail.com>
2026-08-10 19:31:35 +0530

    Complete authentication module

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -3,18 +3,61 @@ from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.models.user import User
-from app.schemas.user import UserLogin
-from app.core.security import verify_password, create_access_token
+from app.schemas.user import UserCreate, UserLogin
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
+from app.core.auth import get_current_user
 
 router = APIRouter()
 
 
+# Register
+@router.post("/register")
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        password=hash_password(user.password),
+        role=user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "data": {
+            "id": new_user.id,
+            "full_name": new_user.full_name,
+            "email": new_user.email,
+            "role": new_user.role
+        }
+    }
+
+
+# Login
 @router.post("/login")
 def login(
     user: UserLogin,
     db: Session = Depends(get_db)
 ):
-
     existing_user = db.query(User).filter(
         User.email == user.email
     ).first()
@@ -43,4 +86,17 @@ def login(
     return {
         "access_token": access_token,
         "token_type": "bearer"
+    }
+
+
+# Current User
+@router.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "full_name": current_user.full_name,
+        "email": current_user.email,
+        "role": current_user.role
     }
--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,15 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.models.user import User
 from app.routers.users import router as user_router
+from app.routers.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CreatorIQ API")
 
 app.include_router(user_router)
+app.include_router(auth_router, prefix="/auth")
+
 
 @app.get("/")
 def home():
